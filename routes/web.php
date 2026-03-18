8ba01f1c644cd0e8f9c39a3a151ae82752df1e9d
NguyenMinh241105 <nguyentrongminhh2005@gmail.com>
2026-03-18 23:31:27 +0700

    Thực hiện yêu cầu 7.6

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,7 @@ Route::get('/nguyentrongminh', function() {
     return 'Nguyễn Trọng Minh '; 
 });
 
-
 // 7.6. Tạo một đường dẫn để hiển thị các bộ phim thuộc thể loại (genre_name) là Action. Nội 
 // dung hiển thị tại file giao diện (view) gồm tên bộ phim (movie_name), ngày phát hành 
-// (release_date), Mô tả (overview), ảnh đại diện (image_link).
-Route::get('/phimaction','App\Http\Controllers\MovieController@phimaction');
+// (release_date), Mô tả (overview), ảnh đại diện (image_link). 
+Route::get('/phimaction', [App\Http\Controllers\MovieController::class, 'phimaction']);
