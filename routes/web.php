4adc915f12f6454f54dc193110c902ea3d1da319
NguyenMinh241105 <nguyentrongminhh2005@gmail.com>
2026-03-18 22:35:20 +0700

    Thêm tên thành viên Trọng Minh

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/nguyenhuygiatoan', function () {
     return 'Nguyễn Huy Gia Toàn';
 });
-
+Route::get('/nguyentrongminh', function() { return 'Nguyễn Trọng Minh'; });
 
