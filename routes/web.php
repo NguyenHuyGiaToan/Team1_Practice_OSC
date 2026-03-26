a054c471c4efca07cbef9dd703f259a3a50cc5f9
NguyenMinh241105 <nguyentrongminhh2005@gmail.com>
2026-03-26 21:10:02 +0700

    Chức năng đăng ký,đăng nhập

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/nguyenhuygiatoan', function () {
@@ -41,3 +42,19 @@ Route::get('/phim-action', [App\Http\Controllers\MovieController::class, 'phimac
 Route::get('/sach','App\Http\Controllers\BookController@sach');
 Route::get('/sach/theloai/{id}','App\Http\Controllers\BookController@theloai');
 Route::get('/sach/chitietsach/{id}',"App\Http\Controllers\BookController@chitiet");
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
